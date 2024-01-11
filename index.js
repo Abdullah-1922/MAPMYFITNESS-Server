b466bfeb9fd763f005b28f91f665615837db4cf6
Abdullah-1922 <kafikafi1922@gmail.com>
2024-01-12 00:24:48 +0600

    save user in database and add like system in blog section

--- index.js
+++ index.js
@@ -4,13 +4,68 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-app.use(cors());
+const cookieParser = require('cookie-parser')
+
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 app.use(express.json());
+app.use(cookieParser())
 
 
-console.log(process.env.DB_USER);
+
+// middlewares
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
+  
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
+app.post('/jwt', async (req, res) => {
+  const user = req.body
+  console.log( user)
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '365d',
+  })
+ 
+     res
+     .cookie('token', token , {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    }).send({ success: true })
+    
+})
 
 
+// Logout
+app.get('/logout', async (req, res) => {
+  try {
+
+    res
+      .clearCookie('token', {
+        maxAge: 0,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+      .send({ success: true })
+    console.log('Logout successful')
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kdy82ie.mongodb.net/?retryWrites=true&w=majority`;
@@ -24,8 +79,11 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 //collections
 const blogCollection = client.db("MAPMYFITNESS").collection("blogs");
+const newsLetterSubCollection = client.db("MAPMYFITNESS").collection("newsletterSub");
+const userCollection = client.db("MAPMYFITNESS").collection("users");
 
 
 async function run() {
@@ -40,52 +98,130 @@ async function run() {
     // await client.close();
   }
 }
+/////////////////////////////////////////////////
+try{
 
-//jwt related api
-app.post('/jwt', async (req, res) => {
-  const user = req.body;
-  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-  res.send({ token })
+
+//Save user to mongodb
+app.put('/users/:email',async(req,res)=>{
+    const user = req.body;
+    const options = { upsert: true };
+    const isExist = await userCollection.findOne({email:user?.email})
+    if(isExist){
+    const result = await userCollection.updateOne({email:user?.email},{ $set: {
+      lastLogin: Date.now()
+    } },options);
+
+
+        return res.send({message:"User already exist"})
+    }
+    const result = await userCollection.insertOne(user);
+    res.send(result);
 })
 
 
-// middlewares
-const verifyToken = (req, res, next) => {
-  // console.log('inside verify token', req.headers.authorization);
-  if (!req.headers.authorization) {
-    return res.status(401).send({ err: true, msg: 'Unauthorized' })
-  }
-  const token = req.headers.authorization.split(' ')[1]
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ err: true, msg: 'Unauthorized' })
-    }
-    req.decoded = decoded
-    next()
-  })
 
-}
+                       //Blog//
 
 
 
-//Blog
-app.post('/blogs',async(req,res)=>{
-  try{
-    const blog =req.body;
+//  add blog 
+app.post('/addBlog', verifyToken,async (req, res) => {
+ 
+    const blog = req.body;
     const result = await blogCollection.insertOne(blog);
     return res.send(result);
-  }catch(err){
-    return res.send({err:true,msg:err});
-  }
+ 
 })
-app.get('/blogs',async(req,res)=>{
-  try{
-    const blogs = await blogCollection.find().toArray();
+
+ // get all blogs
+app.get('/blogs', async (req, res) => {
+  
+    const query = req.query;
+
+    const page = parseInt(query.page);
+    const size = parseInt(query.size);
+
+    const blogs = await blogCollection.find()
+      .skip(page * size)
+      .limit(size)
+      .toArray();
     return res.send(blogs);
-  }catch(err){
-    return res.send({err:true,msg:err});
-    }
+ 
 })
+// get published blog number
+app.get('/blogsCount', async (req, res) => {
+  
+    const count = await blogCollection.estimatedDocumentCount()
+    return res.send({ count });
+ 
+})
+// get single blog
+app.get('/blog/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query = { _id:new ObjectId(id) }
+  console.log(query);
+  const result = await blogCollection.findOne(query);
+  return res.send(result);
+})
+// add like in blog
+app.put('/like',async(req,res)=>{
+  const {id,email}=req.body;
+  console.log(email,'user in like',id);
+  const query = { _id:new ObjectId(id) }
+  const option = { upsert: true };
+  const blog = await blogCollection.findOne(query);
+  const isLiked = blog?.likes?.includes(email);
+  if(isLiked){
+   return res.send ('already liked');
+  }
+  const updateDoc=await blogCollection.updateOne(query,{$push:{likes:email}})
+  return res.send(updateDoc);
+})
+app.put('/unlike',async(req,res)=>{
+  const {id,email}=req.body;
+  console.log(email,'user in like',id);
+  const query = { _id:new ObjectId(id) }
+  const option = { upsert: true };
+  const blog = await blogCollection.findOne(query);
+  const isLiked = blog?.likes?.includes(email);
+  if(isLiked){
+   const updateDoc=await blogCollection.updateOne(query,{$pull:{likes:email}})
+  return res.send(updateDoc);
+  }
+  return res.send ('already unlike');
+})
+// get blog like count
+app.get('/likeCount/:id',async(req,res)=>{
+  const id=req.params.id
+  console.log(id ,'id in the count');
+  const query = { _id: new ObjectId(id) }
+  const result = await blogCollection.findOne(query);
+ 
+  const count = result?.likes?.length;
+  console.log(result?.likes?.length,'result in the count');
+  return res.send(count?.toString());
+})
+
+//NEWSLETTER
+app.post('/newsLetter', async (req, res) => {
+  
+    const newsLetterSub = req.body;
+    const email = newsLetterSub?.email;
+    const result1 = await newsLetterSubCollection.findOne({ email });
+    if (!result1) {
+      const result = await newsLetterSubCollection.insertOne(newsLetterSub);
+      return res.send(result);
+    }
+    return res.send({ err: true, msg: 'Already Subscribed' });
+
+ 
+})
+
+}catch(err){
+  console.log(err);
+}
+
 
 
 
