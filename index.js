fe7333e4fd6c48da8324ab7a44c9289568622ab3
Abdullah-1922 <kafikafi1922@gmail.com>
2024-01-13 13:02:34 +0600

    Add trainer to database complete

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 // middlewares
 const verifyToken = async (req, res, next) => {
   const token = req.cookies?.token
-  
+
   if (!token) {
     return res.status(401).send({ message: 'unauthorized access' })
   }
@@ -35,18 +35,18 @@ const verifyToken = async (req, res, next) => {
 }
 app.post('/jwt', async (req, res) => {
   const user = req.body
-  console.log( user)
+  console.log(user)
   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
     expiresIn: '365d',
   })
- 
-     res
-     .cookie('token', token , {
+
+  res
+    .cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     }).send({ success: true })
-    
+
 })
 
 
@@ -84,6 +84,7 @@ const client = new MongoClient(uri, {
 const blogCollection = client.db("MAPMYFITNESS").collection("blogs");
 const newsLetterSubCollection = client.db("MAPMYFITNESS").collection("newsletterSub");
 const userCollection = client.db("MAPMYFITNESS").collection("users");
+const trainerCollection = client.db("MAPMYFITNESS").collection("trainers");
 
 
 async function run() {
@@ -99,44 +100,46 @@ async function run() {
   }
 }
 /////////////////////////////////////////////////
-try{
+try {
 
 
-//Save user to mongodb
-app.put('/users/:email',async(req,res)=>{
+  //Save user to mongodb
+  app.put('/users/:email', async (req, res) => {
     const user = req.body;
     const options = { upsert: true };
-    const isExist = await userCollection.findOne({email:user?.email})
-    if(isExist){
-    const result = await userCollection.updateOne({email:user?.email},{ $set: {
-      lastLogin: Date.now()
-    } },options);
+    const isExist = await userCollection.findOne({ email: user?.email })
+    if (isExist) {
+      const result = await userCollection.updateOne({ email: user?.email }, {
+        $set: {
+          lastLogin: Date.now()
+        }
+      }, options);
 
 
-        return res.send({message:"User already exist"})
+      return res.send({ message: "User already exist" })
     }
     const result = await userCollection.insertOne(user);
     res.send(result);
-})
+  })
 
 
 
-                       //Blog//
+  //Blog//
 
 
 
-//  add blog 
-app.post('/addBlog', verifyToken,async (req, res) => {
- 
+  //  add blog 
+  app.post('/addBlog', verifyToken, async (req, res) => {
+
     const blog = req.body;
     const result = await blogCollection.insertOne(blog);
     return res.send(result);
- 
-})
 
- // get all blogs
-app.get('/blogs', async (req, res) => {
-  
+  })
+
+  // get all blogs
+  app.get('/blogs', async (req, res) => {
+
     const query = req.query;
 
     const page = parseInt(query.page);
@@ -147,65 +150,94 @@ app.get('/blogs', async (req, res) => {
       .limit(size)
       .toArray();
     return res.send(blogs);
- 
-})
-// get published blog number
-app.get('/blogsCount', async (req, res) => {
-  
+
+  })
+  // get published blog number
+  app.get('/blogsCount', async (req, res) => {
+
     const count = await blogCollection.estimatedDocumentCount()
     return res.send({ count });
- 
-})
-// get single blog
-app.get('/blog/:id',async(req,res)=>{
-  const id = req.params.id;
-  const query = { _id:new ObjectId(id) }
-  console.log(query);
-  const result = await blogCollection.findOne(query);
-  return res.send(result);
-})
-// add like in blog
-app.put('/like',async(req,res)=>{
-  const {id,email}=req.body;
-  console.log(email,'user in like',id);
-  const query = { _id:new ObjectId(id) }
-  const option = { upsert: true };
-  const blog = await blogCollection.findOne(query);
-  const isLiked = blog?.likes?.includes(email);
-  if(isLiked){
-   return res.send ('already liked');
-  }
-  const updateDoc=await blogCollection.updateOne(query,{$push:{likes:email}})
-  return res.send(updateDoc);
-})
-app.put('/unlike',async(req,res)=>{
-  const {id,email}=req.body;
-  console.log(email,'user in like',id);
-  const query = { _id:new ObjectId(id) }
-  const option = { upsert: true };
-  const blog = await blogCollection.findOne(query);
-  const isLiked = blog?.likes?.includes(email);
-  if(isLiked){
-   const updateDoc=await blogCollection.updateOne(query,{$pull:{likes:email}})
-  return res.send(updateDoc);
-  }
-  return res.send ('already unlike');
-})
-// get blog like count
-app.get('/likeCount/:id',async(req,res)=>{
-  const id=req.params.id
-  console.log(id ,'id in the count');
-  const query = { _id: new ObjectId(id) }
-  const result = await blogCollection.findOne(query);
- 
-  const count = result?.likes?.length;
-  console.log(result?.likes?.length,'result in the count');
-  return res.send(count?.toString());
-})
 
-//NEWSLETTER
-app.post('/newsLetter', async (req, res) => {
-  
+  })
+  // get single blog
+  app.get('/blog/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    console.log(query);
+    const result = await blogCollection.findOne(query);
+    return res.send(result);
+  })
+  // add like in blog
+  app.put('/like', async (req, res) => {
+    const { id, email } = req.body;
+
+    const query = { _id: new ObjectId(id) }
+    const option = { upsert: true };
+    const blog = await blogCollection.findOne(query);
+    const isLiked = blog?.likes?.includes(email);
+    if (isLiked) {
+      return res.send('already liked');
+    }
+    const updateDoc = await blogCollection.updateOne(query, { $push: { likes: email } })
+    return res.send(updateDoc);
+  })
+  app.put('/unlike', async (req, res) => {
+    const { id, email } = req.body;
+
+    const query = { _id: new ObjectId(id) }
+    const option = { upsert: true };
+    const blog = await blogCollection.findOne(query);
+    const isLiked = blog?.likes?.includes(email);
+
+
+    if (isLiked) {
+      const updateDoc = await blogCollection.updateOne(query, { $pull: { likes: email } })
+      return res.send(updateDoc);
+    }
+    return res.send('already unlike');
+  })
+
+  // get blog like count
+  app.get('/likeCount/:id', async (req, res) => {
+    const id = req.params.id
+
+
+    const query = { _id: new ObjectId(id) }
+
+    const result = await blogCollection.findOne(query);
+
+    const count = result?.likes?.length;
+
+    return res.send(count?.toString());
+
+
+
+  })
+
+
+  app.get('/checkLike/:id',async(req,res)=>{
+
+    const id=req.params.id
+    const query={_id:new ObjectId(id)}
+    const result=await blogCollection.findOne(query)
+    const email=req.query.email
+   
+    const isLiked=result?.likes?.includes(email)
+    if(isLiked){
+      return res.send(true)
+    }
+    return res.send(false)
+  })
+
+  //Get blog for home page
+  app.get('/homeblog',async(req,res)=>{
+    const result = await blogCollection.find().sort({timestampField:-1}).limit(3).toArray();
+   return res.send(result)
+  })
+
+  //NEWSLETTER
+  app.post('/newsLetter', async (req, res) => {
+
     const newsLetterSub = req.body;
     const email = newsLetterSub?.email;
     const result1 = await newsLetterSubCollection.findOne({ email });
@@ -215,10 +247,26 @@ app.post('/newsLetter', async (req, res) => {
     }
     return res.send({ err: true, msg: 'Already Subscribed' });
 
- 
-})
 
-}catch(err){
+  })
+
+  // Trainer related Api
+
+  //add trainer
+
+  app.post('/addTrainer',async(req,res)=>{
+    const trainer=req.body
+    isExist =await trainerCollection.findOne({email:trainer?.email})
+    if(isExist){
+      return res.send({err:true,msg:'Trainer already exist'})}
+    const result=await trainerCollection.insertOne(trainer)
+    return res.send(result)
+  })
+
+
+
+
+} catch (err) {
   console.log(err);
 }
 
